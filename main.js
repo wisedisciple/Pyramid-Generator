c95e197cd292b0c52e44a6efdcad17644c156b7d
Chris Roberts <60993035+wisedisciple@users.noreply.github.com>
2025-01-14 09:43:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+// set variables
 const character = "#";
 const count = 8;
 const rows = [];
 let inverted = true;
 
+// put the character and spaces for returning
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+//loop over the item for count
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
@@ -21,4 +24,5 @@ for (const row of rows) {
   result = result + row + "\n";
 }
 
+//show the pyramid
 console.log(result);
